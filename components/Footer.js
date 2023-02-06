24f027f3f3f32417aa649b2c1a58cfac02fc663a
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2023-02-06 02:28:09 -0500

    Fixed margin for Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            <div className='container flex justify-center items-center mx-auto opacity-50 text-sm'>
+            <div className='container flex justify-center items-center mx-auto my-6 opacity-50 text-sm'>
                 &copy; {new Date().getFullYear()} Placeholder
             </div>
         </footer>
